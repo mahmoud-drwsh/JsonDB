9c12866e29275095fb4ac8cf282cdd9738a1aacc
mahmoud <mahmoud@MAHMOUD-Y700>
2017-02-19 16:49:32 +0700

    Add project files.

---
+++ JsonDatabase/JsonDatabase.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JsonDatabase
+{
+    public class Database
+    {
+        private const string JsonExtension = "json";
+
+        private string DatabaseDirectory { get; set; }
+
+        private JsonSerializer Serializer { get; set; } = new JsonSerializer() { Formatting = Formatting.Indented };
+
+        public Database()
+        {
+            DatabaseDirectory = Environment.CurrentDirectory;
+            DatabaseDirectory = Path.Combine(DatabaseDirectory, "Data");
+        }
+
+        public Database(string DatabaseDirectory)
+        {
+            this.DatabaseDirectory = DatabaseDirectory;
+        }
+
+        public Database(string DatabaseDirectory, JsonSerializer Serializer)
+        {
+            this.DatabaseDirectory = DatabaseDirectory;
+            this.Serializer = Serializer;
+        }
+
+        public List<T> GetCollection<T>()
+        {
+            var Collection = new List<T>();
+
+            try
+            {
+                using (var sr = new StreamReader(GetPath<T>()))
+                using (var jtr = new JsonTextReader(sr))
+                {
+                    Collection.AddRange(Serializer.Deserialize<List<T>>(jtr));
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("GetCollection<T>():");
+                Console.WriteLine(ex.Message);
+
+                File.Create(ex.FileName).Dispose();
+
+                GetCollection<T>();
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("GetCollection<T>():");
+                Console.WriteLine(ex.Message);
+                Directory.CreateDirectory(Path.GetDirectoryName(GetPath<T>()));
+                GetCollection<T>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetCollection<T>():");
+                Console.WriteLine(ex.Message);
+            }
+
+            return Collection;
+        }
+
+        public void Insert<T>(T Item)
+        {
+            var Collection = GetCollection<T>();
+
+            Collection.Add(Item);
+
+            Serialize(Collection);
+        }
+
+        public void Insert<T>(IEnumerable<T> Item)
+        {
+            var Collection = GetCollection<T>();
+            Collection.AddRange(Item);
+
+            Serialize(Collection);
+        }
+
+        public int Delete<T>(Predicate<T> Match)
+        {
+            var Collection = GetCollection<T>();
+
+            Collection.RemoveAll(Match);
+
+            Serialize(Collection);
+
+            return Collection.Count();
+        }
+
+        private void Serialize<T>(IEnumerable<T> List)
+        {
+            try
+            {
+                using (var streamWriter = new StreamWriter(GetPath<T>()))
+                {
+                    using (var jsonTextWriter = new JsonTextWriter(streamWriter))
+                    {
+                        Serializer.Serialize(jsonTextWriter, List);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Serialize<T>():");
+                Directory.CreateDirectory(Path.GetDirectoryName(GetPath<T>()));
+                Serialize(List);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Serialize<T>():");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private string GetPath<T>()
+        {
+            return Path.ChangeExtension(Path.Combine(DatabaseDirectory, typeof(T).Name.Pluralize().Capitalize()), JsonExtension);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# JsonDatabase
+
+## consider the Following 
+
+```csharp
+class File {
+public string Name { get; set; }
+}
+```
+
+## Examples
+
+```csharp
+using JsonDatabse;
+...
+
+//provide a directory for the database to save .json files to
+//if no directory is provided the directory will be ".\Data\"
+var db = new Database(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+//just start inserting anything and it will create a .json file for that specific type in the directory given above
+//notice that a type is required
+db.Insert<File>(new File() { Name = "NewFile" });
+//you can also insert a list of Files
+db.Insert<File>(new List<File>() { new File() { Name = "File1" }, new File() { Name = "File2" } });
+
+//when you call GetCollection<T>(); it will return a List<T> filled with what was inserted of the type given
+var FilesCollection = db.GetCollection<File>();
+
+//to delete a record you have to use linq just provide proper filter to delete
+//all matching items will be deleted
+db.Delete<File>(f => f.Name == "NewFile");
+
+```
