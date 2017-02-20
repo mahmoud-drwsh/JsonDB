976cc3af50d21441d8216c13b49a0b6efe53aa8e
mahmoud darweesh <mahmoud darweesh>
2017-02-20 20:24:21 +0700

    Added JsonList<T>

--- JsonDB/JsonDB.cs
+++ JsonDB/JsonDB.cs
@@ -9,99 +9,77 @@ using Inflector;
 
 namespace JsonDB {
 	public class Database {
+
+		#region Properties
+
 		private const string JsonExtension = "json";
 
-		private string DatabaseDirectory { get; set; }
+		public string DatabaseDirectory { get; set; }
 
-		private JsonSerializer Serializer { get; set; } = new JsonSerializer() { Formatting = Formatting.Indented };
+		private JsonSerializer Serializer { get; set; }
+
+		#endregion
+
+		#region Constructors
 
 		public Database() {
-			DatabaseDirectory = Environment.CurrentDirectory;
-			DatabaseDirectory = Path.Combine(DatabaseDirectory, "Data");
+			DatabaseDirectory = Path.Combine(Environment.CurrentDirectory, "Database");
+			Serializer = new JsonSerializer();
 		}
 
-		public Database(string DatabaseDirectory) {
-			this.DatabaseDirectory = DatabaseDirectory;
+		public Database(string databaseDirectory, JsonSerializer serializer) {
+			DatabaseDirectory = databaseDirectory;
+			Serializer = serializer;
 		}
 
-		public Database(string DatabaseDirectory, JsonSerializer Serializer) {
-			this.DatabaseDirectory = DatabaseDirectory;
-			this.Serializer = Serializer;
+		public Database(string databaseDirectory) {
+			DatabaseDirectory = databaseDirectory;
+			Serializer = new JsonSerializer();
 		}
 
-		public List<T> GetCollection<T>() {
-			var Collection = new List<T>();
+		#endregion
+
+		public JsonList<T> GetCollection<T>() {
+			var Collection = new JsonList<T>(this);
 
 			try {
-				using(var sr = new StreamReader(GetPath<T>()))
-				using(var jtr = new JsonTextReader(sr)) {
-					Collection.AddRange(Serializer.Deserialize<List<T>>(jtr));
+				using(var streamWriter = new StreamReader(GetPath<T>()))
+				using(var jsonTextReader = new JsonTextReader(streamWriter)) {
+					Collection.AddRange(Serializer.Deserialize<List<T>>(jsonTextReader));
 				}
 			} catch(FileNotFoundException ex) {
-				Console.WriteLine("GetCollection<T>():");
-				Console.WriteLine(ex.Message);
-
 				File.Create(ex.FileName).Dispose();
-
 				GetCollection<T>();
-			} catch(DirectoryNotFoundException ex) {
-				Console.WriteLine("GetCollection<T>():");
-				Console.WriteLine(ex.Message);
+			} catch(DirectoryNotFoundException) {
 				Directory.CreateDirectory(Path.GetDirectoryName(GetPath<T>()));
 				GetCollection<T>();
-			} catch(Exception ex) {
-				Console.WriteLine("GetCollection<T>():");
-				Console.WriteLine(ex.Message);
+			} catch(Exception) {
+
 			}
 
 			return Collection;
 		}
 
-		public void InsertOne<T>(T Item) {
-			var Collection = GetCollection<T>();
-
-			Collection.Add(Item);
-
-			Serialize(Collection);
-		}
-
-		public void InsertMany<T>(IEnumerable<T> Item) {
-			var Collection = GetCollection<T>();
-
-			Collection.AddRange(Item);
-
-			Serialize(Collection);
-		}
-
-		public int Delete<T>(Predicate<T> Match) {
-			var Collection = GetCollection<T>();
-
-			Collection.RemoveAll(Match);
-
-			Serialize(Collection);
-
-			return Collection.Count();
-		}
-
-		private void Serialize<T>(IEnumerable<T> List) {
+		internal void SaveToDisk<T>(IEnumerable<T> List) {
 			try {
-				using(var streamWriter = new StreamWriter(GetPath<T>())) {
-					using(var jsonTextWriter = new JsonTextWriter(streamWriter)) {
-						Serializer.Serialize(jsonTextWriter, List);
-					}
+				using(var streamWriter = new StreamWriter(GetPath<T>()))
+				using(var jsonTextWriter = new JsonTextWriter(streamWriter)) {
+					Serializer.Serialize(jsonTextWriter, List);
 				}
 			} catch(DirectoryNotFoundException) {
-				Console.WriteLine("Serialize<T>():");
 				Directory.CreateDirectory(Path.GetDirectoryName(GetPath<T>()));
-				Serialize(List);
-			} catch(Exception ex) {
-				Console.WriteLine("Serialize<T>():");
-				Console.WriteLine(ex.Message);
+				SaveToDisk(List);
+			} catch(Exception) {
+
 			}
 		}
+
+		#region Helpers
 
 		private string GetPath<T>() {
 			return Path.ChangeExtension(Path.Combine(DatabaseDirectory, typeof(T).Name.Pluralize().Capitalize()), JsonExtension);
 		}
+
+		#endregion
 	}
 }
---
+++ JsonDB/JsonList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace JsonDB {
+	public class JsonList<T> : List<T> {
+
+		private Database DB { get; set; }
+
+		public JsonList(Database dB) {
+			DB = dB;
+		}
+
+		public void Save() {
+			DB.SaveToDisk(this);
+		}
+	}
+}
